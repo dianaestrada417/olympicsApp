f085be0c0e32775adafa9097e08c2ad202505fbe
dianaestrada417 <estrada.diana120@gmail.com>
2024-10-06 00:55:45 -0700

    inital commit

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator();
+
+//Page 1 Component
+function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Home Page - LA28 Olympics!</Text>
+    </View>
+  );
+}
+
+// Page 2 Component
+function EventsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Events Page - Check out the upcoming events!</Text>
+    </View>
+  );
+}
+
+// Page 3 Component
+function AthletesScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Athletes Page - Meet the participants!</Text>
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Events" component={EventsScreen} />
+        <Drawer.Screen name="Athletes" component={AthletesScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
