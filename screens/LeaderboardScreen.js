d7b2e2f7250037ee77958fe9be435525eddc7abf
dianaestrada417 <estrada.diana120@gmail.com>
2024-10-16 16:22:07 -0700

    changes to leaderboard

--- screens/LeaderboardScreen.js
+++ screens/LeaderboardScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { View, Text, FlatList, StyleSheet, Button } from 'react-native';
 import { db } from '../firebase'; // Ensure this path is correct
 import { collection, onSnapshot } from 'firebase/firestore';
 
@@ -21,8 +21,23 @@ const LeaderBoard = () => {
     return () => unsubscribe();
   }, []);
 
+  const handleRedeemPoints = () => {
+    console.log("Redeem Points button clicked!");
+    // Here you can add any additional logic for redeeming points if necessary
+  };
+
   return (
     <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerTitle}>GreenPoints LeaderBoard🏆</Text>
+        <Text style={styles.headerText}>
+          Earn points by exploring and completing eco-friendly challenges throughout Los Angeles{"\n"}
+        </Text>
+      </View>
+      <Button title="Redeem Points" onPress={handleRedeemPoints} />
+        <Text style={styles.subText}>
+          Get coupons to small businesses in the LA area!
+        </Text>
       <FlatList
         data={users}
         keyExtractor={(item) => item.id} // Ensure each item has a unique key
@@ -54,10 +69,48 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
   evenRow: {
-    backgroundColor: '#e6f7ff', // Light blue background for even rows
+    backgroundColor: '#c7f6b6', // Light blue background for even rows
   },
   oddRow: {
     backgroundColor: '#fff', // White background for odd rows
+  },
+  headerContainer: {
+    width: '100%',
+    padding: 20,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    marginVertical: 15,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+  },
+  headerTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#003366',
+    textAlign: 'center',
+  },
+  headerText: {
+    fontSize: 14,
+    lineHeight: 20,
+    color: '#333',
+    textAlign: 'center',
+  },
+
+  subText: {
+    fontSize: 12,
+    color: '#555',
+    textAlign: 'center',
+    marginBottom: 10,
   },
 });
 
